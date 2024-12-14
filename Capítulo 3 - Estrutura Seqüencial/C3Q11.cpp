9962a32174e987462e8c03585e9e554722368b85
RafBal <rafael501baldin@gmail.com>
2024-12-14 08:04:06 +0100

    Exercício 11

--- Capítulo 3 - Estrutura Seqüencial/C3Q11.cpp
+++ Capítulo 3 - Estrutura Seqüencial/C3Q11.cpp
@@ -13,10 +13,12 @@ int main(void)
         std::cin >> num;
     }
     quad = pow(num, 2);
+    std::cout << "O número " << num << " ao quadrado é: " << quad << std::endl;
     cubo = pow(num, 3);
+    std::cout << "O número " << num << " ao cubo é: " << cubo << std::endl;
     r2 = sqrt(num);
+    std::cout << "A raíz quadrada do número " << num << " é: " << r2 << std::endl;
     r3 = cbrt(num);
-    
-
+    std::cout << "A raíz cúbica do número " << num << " é: " << r3 << std::endl;
     return 0;
 }
